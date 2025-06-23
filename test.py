7f504b83bffcb5ad17037f44b2f4ff5692185e49
Alexey Boyko <boyko.phystech@gmail.com>
2025-06-23 18:44:53 +0300

    n_samples made explicit, not hardcoded

--- test.py
+++ test.py
@@ -11,7 +11,7 @@ from cadrille import Cadrille, collate
 from dataset import Text2CADDataset, CadRecodeDataset
 
 
-def run(data_path, split, mode, checkpoint_path, py_path):
+def run(data_path, split, mode, checkpoint_path, py_path, n_samples=1):
     # should be no predicted codes from previous experiments
     os.makedirs(args.py_path, exist_ok=True)
     assert len(os.listdir(py_path)) == 0
@@ -47,12 +47,11 @@ def run(data_path, split, mode, checkpoint_path, py_path):
             mode=mode)
         batch_size = 256
 
-    n_samples = 1
     counter = 0
     dataloader = DataLoader(
         dataset=ConcatDataset([dataset] * n_samples),
         batch_size=batch_size,
-        num_workers=16,
+        num_workers=24,
         collate_fn=partial(collate, processor=processor, n_points=256, eval=True))
 
     for batch in tqdm(dataloader):
@@ -85,5 +84,6 @@ if __name__ == '__main__':
     parser.add_argument('--mode', type=str, default='pc')
     parser.add_argument('--checkpoint-path', type=str, default='maksimko123/cadrille')
     parser.add_argument('--py-path', type=str, default='./work_dirs/tmp_py')
+    parser.add_argument('--n-samples', type=int, default=1)
     args = parser.parse_args()
-    run(args.data_path, args.split, args.mode, args.checkpoint_path, args.py_path)
+    run(args.data_path, args.split, args.mode, args.checkpoint_path, args.py_path, args.n_samples)
