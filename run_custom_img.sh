843217a9d9f20c335f7fe54e185be6e36ab281b8
Alexey Boyko <boyko@airi.net>
2025-06-24 21:36:49 +0300

    script for img mode

---
+++ run_custom_img.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+PARENT_DIR="/home/jovyan/users/zhemchuzhnikov/boyko/part_sampling_expanded/data/core_mechanical_engineering_dataset/"
+OUTPUTS_DIR=outputs_2_img
+
+# Loop through all subfolders in the parent directory
+for dir in "$PARENT_DIR"/*/; do
+    if [ -d "$dir" ]; then
+        name_i=$(basename "$dir")
+        echo "Processing $name_i..."
+        #python test.py --data "$name_i" --output_folder "./outputs/$name_i"
+        python test.py --data-path ../part_sampling_expanded/data/core_mechanical_engineering_dataset/ \
+        --split "$name_i" --mode img --n-samples 1 --py-path "./$OUTPUTS_DIR/py_files/$name_i"
+    fi
+done
